782a575b44629d5fd871300f381b4cb19ba7e4fd
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2022-04-28 14:11:49 +0100

    accomplished the first steps of the project : 1, 2 & 3

---
+++ projet/anlayse.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+function show_usage(){
+	echo "logs.sh:[-h] [-m] [-g] [-c] [-l] [-a] [-b] chemin.."
+}
+function tester(){
+	if [[ $1 == "-h" || $1 == "-m" ||  $1 == "-g" ||  $1 == "-c" || $1  == "-l" || $1 == "-a" || $1 == "-b" ]]
+		then echo "1"
+		else 
+			echo "error of usage of arguments for log.sh"
+	fi
+			
+}
+function help(){
+	echo "Description :"
+	echo "==================================================================="
+	echo " 	 1. analysé le systeme de connexion des users sur le Linux Kernel
+	 2. exection de cette application consiste le super User (root)
+	 -h => afficher le manuel d'utilisation de l'application
+	 -c => tentaive de connexion
+	 -a => alert
+	 -l => envoyer une notification au users via un email
+	 -b => starting name service
+	 -g => afficher un menu graphic UI
+	 -m => afficher un menu textuelle"
+echo "==================================================================="
+}
+function connexion(){
+	file="log.txt"
+	tentative=0
+
+}
+show_usage
+read REP
+tester $REP
+help
+
